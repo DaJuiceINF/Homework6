6e3f029853ed07a7b3a3dd775cf4682807b450d3
Mr. P Nedzingahe <u14285445@up.ac.za>
2019-03-18 16:57:29 +0200

    My commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
+import {ReportingService} from './reporting.service'
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatSelectModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { HttpClient } from 'selenium-webdriver/http';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule, MatSelectModule
   ],
-  providers: [],
+  providers: [ReportingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
